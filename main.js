b45b88afde65f625e4c0708ad414df5355e79ccb
LuisAlvarez99 <luis.alvarez0129@outlook.com>
2019-11-17 16:40:46 -0500

    added new sections that will be assigned on click functions. next start creating functioning JS.

---
+++ main.js
@@ -0,0 +1,11 @@
+// let pkmnSelect = document.getElementById('pkmn-select')
+//
+// let pkmnDiv = document.getElementById('pkmn-img')
+// const img = document.createElement('img')
+// pkmndiv.appendChild
+// img.src = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${p}.png`
+// let p = pkmnSelect.value
+//
+// let pkmnPic = url(`https://assets.pokemon.com/assets/cms2/img/pokedex/full/${p}.png`)
+//
+// console.log(p)
