13da1a76cfdfa1a3100674f4c4b5dc7f10164b94
Luis Alvarez <luisalvarez@Luiss-Air.home>
2019-11-22 01:31:55 -0500

    added on click attributes in JS

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-let thing = document.getElementById('section')
+let section = document.getElementById('section')
+let moveList = document.getElementById('move-list')
+let summary = document.getElementById('move-sum')
 let team = document.getElementById('team-container')
 let player = document.getElementsByClassName('team-member')
 let btn = document.getElementById('team-btn')
@@ -6,17 +8,28 @@ let btn = document.getElementById('team-btn')
 // link to trainer sprites for men (http://www.pokestadium.com/assets/img/tools/trainercard/trainers/male/177.png)
 // link to trainer sprites for women (http://www.pokestadium.com/assets/img/tools/trainercard/trainers/female/109.png)
 
+function showDiv() {
+  section.style.display = 'block'
+}
+
+function showMoves() {
+  summary.style.display = 'block'
+}
+section.setAttribute('onclick', 'showMoves()')
+
 btn.addEventListener('click', () => {
   let player = prompt('enter your name')
   let gen = prompt('male of female?')
   let x = prompt('enter trainer number(1-109)')
   let span = document.createElement('span')
+  span.setAttribute('onclick','showDiv()')
   span.className = 'team-member'
   span.innerHTML = `<h3>${player}</h3>
                     <img src="http://www.pokestadium.com/assets/img/tools/trainercard/trainers/${gen}/${x}.png" alt="" width='33%' height='auto'>
-  `
+                    `
   team.appendChild(span)
 })
+
 
 
 
@@ -32,8 +45,6 @@ queryPokemonAPI = async (val) => {
   let req =  await fetch(`https://pokeapi.co/api/v2/pokemon/${val}`)
   let data =  await req.json()
 
-
-
   let poke = {
     link: data.sprites.front_default,
     name: data.name,
@@ -46,7 +57,7 @@ queryPokemonAPI = async (val) => {
 
   for(let i in data.abilities) {
     let aName = data.abilities[i].ability.name
-    poke.abil += aName
+    poke.abil = aName
     console.log(aName)
   }
   drawPkmn(poke)
@@ -73,5 +84,11 @@ class Trainer {
   }
   addPokemon() {
     this.pokemon.push()
+  }
+}
+
+class Pokemon {
+  constructor() {
+
   }
 }
